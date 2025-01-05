ac308d013830a532a4d3a3046c973c2891ad6aba
Vijay Kumar Kushwaha <vijay77495@gmail.com>
2025-01-05 10:08:39 +0530

    commit

---
+++ src/main/java/com/hib/main/Main.java
@@ -0,0 +1,12 @@
+package com.hib.main;
+
+import com.hib.entity.Student;
+import com.hib.entity.StudentDetails;
+
+public class Main {
+	public static void main(String[] args) {
+		Student s = new Student("saurav","saurav@gmail.com",983824);
+		StudentDetails sd = new StudentDetails("MG Road", "kolata",542352);
+	 
+
+}
